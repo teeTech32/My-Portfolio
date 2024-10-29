bf53ad222282fea2f5215b5c92ac5947d668d2eb
Atanda Timothy <timothyabidemi4618@gmail.com>
2024-10-30 00:05:50 +0100

    Add project

---
+++ src/data.js
@@ -0,0 +1,180 @@
+import css3 from "./assets/css3.svg";
+import HTML5 from "./assets/html.svg";
+import Javascript from "./assets/javascript.svg";
+import Mongodb from "./assets/mongodb.svg";
+import FastAPI from "./assets/FastAPI.svg";
+import Reactjs from "./assets/reactjs.svg";
+import Github2 from "./assets/Github2.jpg";
+import TailwindCSS from "./assets/tailwindcss.svg"
+import bootstrap from "./assets/bootstrap-svgrepo-com.svg";
+import aws from "./assets/aws-svgrepo-com.svg";
+import docker from "./assets/docker-svgrepo-com (1).svg";
+import firebase from "./assets/firebase-svgrepo-com.svg";
+import postgres from "./assets/postgresql-logo-svgrepo-com (1).svg";
+import git from "./assets/git.svg"
+import portfolio from "./assets/portfolio.jpg"
+import Homes from "./assets/Homes.jpg"
+import LoanC from "./assets/LoanC.jpg"
+import contact from "./assets/contact.jpg"
+import vite from './assets/vite-svgrepo-com.svg'
+import express from './assets/express.svg'
+import Ecommerce1 from './assets/Ecommer1.jpg'
+
+export const tools = [
+  {
+    name: "React",
+    icon: Reactjs,
+  },
+  {
+    name: "JavaScript",
+    icon: Javascript,
+  },
+  {
+    name: "HTML5",
+    icon: HTML5,
+  },
+  {
+    name: "CSS3",
+    icon: css3,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+  {
+    name: "TailwindCSS",
+    icon: TailwindCSS
+  },
+  {
+    name: "Docker",
+    icon: docker,
+  },
+  {
+    name: "FireBase",
+    icon: firebase,
+  },
+  {
+    name: "AWS",
+    icon: aws,
+  },
+  {
+    name: "PostgreSQL",
+    icon: postgres,
+  },
+  {
+    name: "FastAPI",
+    icon: FastAPI
+  },
+  {
+    name: "MongoDB",
+    icon: Mongodb,
+  },
+   {
+    name: "Express",
+    icon: express,
+  },
+   {
+    name: "Vite",
+    icon:vite,
+  },
+];
+
+export const skills = [
+  {
+    name: "ReactJS",
+    value: 78,
+    icon: Reactjs,
+  },
+  {
+    name: "JavaScript",
+    value: 70,
+    icon: Javascript,
+  },
+  {
+    name: "HTML5",
+    value: 80,
+    icon: HTML5,
+  },
+  {
+    name: "TailwindCSS",
+    value: 75,
+    icon: TailwindCSS
+  },
+  {
+    name: "CSS3",
+    value: 70,
+    icon: css3,
+  },
+  {
+    name: "MongoDB",
+    value: 68,
+    icon: Mongodb,
+  },
+  {
+    name: "FastAPI",
+    value: 65,
+    icon: FastAPI,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "teeWears",
+    cat: "Web Design",
+    url: "https://ecommerce1-orpin.vercel.app/",
+    img: Ecommerce1,
+  },
+  {
+    id: 2,
+    title: "ABC Homes and Properties",
+    cat: "Web Design",
+    url: "https://homes-and-properties.vercel.app/",
+    img: Homes,
+  },
+   {
+    id: 3,
+    title: "Contact Saving App",
+    cat: "Mobile and Web App",
+    url: "https://contacts-frontend-livid.vercel.app/",
+    img: contact,
+  },
+ 
+  {
+    id: 4,
+    title: "Github User Fetcher",
+    cat: "Mobile and Web app",
+    url: "https://github-fetcher-six.vercel.app/",
+    img: Github2,
+  },
+  {
+    id: 5,
+    title: "My Portfolio",
+    cat: "Web Design",
+    url: "",
+    img: portfolio,
+  },
+  {
+    id: 6,
+    title: "Loan Calculator",
+    cat: "Mobile and Web App",
+    url: "https://loan-calculator-rouge.vercel.app/",
+    img: LoanC,
+  },
+ 
+];
+
+export const softskills = [
+  "Time management",
+  "Communication",
+  "Teamwork",
+  "Creativity",
+  "Self-learning",
+  "Accountability",
+  "Problem solving",
+  "Open-mindedness",
+];
